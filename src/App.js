8a14a85facb1ba7b35e86abdab5ec9ad5e73634f
katefranks <kmfranks9@gmail.com>
2021-06-01 19:02:09 -0400

    assignment completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import logo from './logo.svg';
+import {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      likes: 0,
+    }
+
+    this.incrementLikes = this.incrementLikes.bind(this);
+  }
+
+  incrementLikes = () => {
+    let likesTotal = this.state.likes + 1
+    this.setState({
+      likes: likesTotal
+    })
+  }
+
+  render() {
+    return (
+      <div className="button-container">
+        <button className="like-button" onClick={this.incrementLikes}>{this.state.likes}{this.state.likes === 1 ? ' Like \uD83D\uDE0D' : ' Likes \uD83D\uDE0D'}
+        </button>
+      </div>
+    )
+  }
 }
 
 export default App;
